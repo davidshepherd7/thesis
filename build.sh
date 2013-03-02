915e878621208141c9b45ac8a59df70b0290e34e
David Shepherd <davidshepherd7@gmail.com>
2013-03-02 17:57:58 +0000

    Swapped to biblatex and biber (for bibliographys)
    We can now use utf8 properly so also fixed (most of...) the author names to
    use proper utf characters where needed.
    
    Fixed websites to use the url and urldate fields (added by biblatex).

--- build.sh
+++ build.sh
@@ -3,10 +3,13 @@
 set -o errexit
 set -o nounset
 
+# Get rid of the bibtex cache, causes problems sometimes
+touch main.bbl && rm main.bbl
+
 latex_command="pdflatex -halt-on-error -interaction=nonstopmode -file-line-error -output-format pdf main.tex"
-bibtex_command="bibtex main.aux"
+bibliography_command="biber main.bcf" # note: biber uses *.bcf not *.aux
 
 $latex_command
-$bibtex_command
+$bibliography_command
 $latex_command
-$latex_command
+# Only need to run latex once after bibtex because we are using biblatex
