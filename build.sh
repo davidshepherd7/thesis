7d265f08b38f933b7d32f1931fd9030d969da252
David Shepherd <davidshepherd7@gmail.com>
2013-03-02 18:46:50 +0000

    Removed some references to removed sections
    Also force bibliography backend to be biber (so that it handles utf8 ok).
    
    '#' will be ignored, and an empty message aborts the commit.  # On branch
    master # Changes to be committed: # (use "git reset HEAD <file>..." to
    unstage) # # modified: build.sh # modified: introduction.tex # modified:
    main.tex # modified: modelling_basics.tex #

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 
 # Get rid of the bibtex cache, causes problems sometimes
-touch main.bbl && rm main.bbl
+# touch main.bbl && rm main.bbl
 
 latex_command="pdflatex -halt-on-error -interaction=nonstopmode -file-line-error -output-format pdf main.tex"
 bibliography_command="biber main.bcf" # note: biber uses *.bcf not *.aux
